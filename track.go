ef9b2096c29c7d4ab85309bc6844df7b668f4035
Thomas Prochazka <thomas.prochazka@gmail.com>
2017-12-08 13:44:04 +0100

    Work on implementing the list command for interruptions

--- track.go
+++ track.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/mitchellh/go-homedir"
 )
+
+var interruptDateFormat = "2006-01-02 15:04:05"
 
 type InterruptEntry struct {
 	date  time.Time
@@ -16,57 +20,95 @@ type InterruptEntry struct {
 }
 
 func (entry InterruptEntry) String() string {
-	return fmt.Sprintf("%s: %s", entry.date.Format("2006-01-02 15:04:05"), entry.label)
+	return fmt.Sprintf("%s - %s", entry.date.Format(interruptDateFormat), entry.label)
+}
+
+func parse(line string) InterruptEntry {
+	parts := strings.Split(line, " - ")
+	dateStr := parts[0]
+	labelStr := parts[1]
+
+	parsedDate, _ := time.Parse(interruptDateFormat, dateStr)
+
+	return InterruptEntry{label: labelStr, date: parsedDate}
 }
 
 func main() {
-	fmt.Printf("Hello World")
-
 	if len(os.Args) < 2 {
 		fmt.Println("Invalid number of arguments")
 	}
 
-	var isInterrupt = flag.Bool("i", false, "Create a new interrupt")
-	flag.Parse()
-
-	fmt.Println("interrupt has value ", *isInterrupt)
-
-	if *isInterrupt {
-		createInterrupt("test")
-	}
-
 	// Usage:
 	// track -i "XXX" # Track an interrupt with label "XXX"
+	// track list
 	// track "XXX" # Add track event with label "XXX"
 	// track --stop # Add stop track event
-}
 
-func createInterrupt(label string) {
-	var dir = ensureTrackFolder()
-	var filename = ensureTrackFile(dir)
-	var entry = createInterruptEntry(label)
-
-	fmt.Println("Create interrupt ", label, dir, entry)
-
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	if _, err = fmt.Fprintf(f, "%s\n", entry.String()); err != nil {
-		panic(err)
+	switch os.Args[1] {
+	case "list":
+		executeListCommand()
+	default:
+		executeGeneralCommand()
 	}
 }
 
-func ensureTrackFolder() string {
-	var dir, err = homedir.Dir()
+func executeListCommand() {
+	data := readInterruptData()
+
+	for _, entry := range data {
+		fmt.Println(entry.String())
+	}
+}
+
+func readInterruptData() []InterruptEntry {
+	dir := ensureTrackFolder()
+	interruptFilename := ensureInterruptFile(dir)
+
+	file, err := os.Open(interruptFilename)
 	if err != nil {
 		fmt.Println("oh no ", err)
 	}
 
-	var trackDir = path.Join(dir, ".track")
+	defer file.Close()
+
+	var entries []InterruptEntry
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		entry := parse(line)
+		entries = append(entries, entry)
+	}
+
+	return entries
+}
+
+func executeGeneralCommand() {
+	isInterrupt := flag.Bool("i", false, "Create a new interrupt")
+	flag.Parse()
+
+	if *isInterrupt {
+		createInterrupt("test")
+	}
+}
+
+func createInterrupt(label string) {
+	dir := ensureTrackFolder()
+	trackFilename := ensureTrackFile(dir)
+	interruptFilename := ensureInterruptFile(dir)
+	entry := createInterruptEntry(label)
+
+	writeInterruptEntry(entry, trackFilename)
+	writeInterruptEntry(entry, interruptFilename)
+}
+
+func ensureTrackFolder() string {
+	dir, err := homedir.Dir()
+	if err != nil {
+		fmt.Println("oh no ", err)
+	}
+
+	trackDir := path.Join(dir, ".track")
 	err = os.MkdirAll(trackDir, os.ModePerm)
 	if err != nil {
 		fmt.Println("oh no ", err)
@@ -77,10 +119,27 @@ func ensureTrackFolder() string {
 }
 
 func ensureTrackFile(dir string) string {
-	var filename = time.Now().Format("2006-01-02")
+	filename := time.Now().Format("2006-01-02")
 	return path.Join(dir, filename)
+}
+
+func ensureInterruptFile(dir string) string {
+	return path.Join(dir, "_interrupts")
 }
 
 func createInterruptEntry(label string) InterruptEntry {
 	return InterruptEntry{label: label, date: time.Now()}
+}
+
+func writeInterruptEntry(entry InterruptEntry, filename string) {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	if _, err = fmt.Fprintf(f, "%s\n", entry.String()); err != nil {
+		panic(err)
+	}
 }
